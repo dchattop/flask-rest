15b0bd0510c6227466e2549fa4d6fa65afe88834
blackmufasa <93259458+blackmufasa@users.noreply.github.com>
2022-03-10 17:37:36 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,13 +1,24 @@
-project_home = '/app'
-project_temp = '/app/temp'
-project_sql = '/app/sql'
+from pathlib import Path
+import os
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+ROOT_FILE=os.path.join(BASE_DIR, 'root.crt')
+
+project_home = BASE_DIR
+project_temp = os.path.join(BASE_DIR, 'temp')
+project_sql = os.path.join(BASE_DIR, 'sql')
+
+NAME = os.environ.get('JDV_SCHEMA')
+USER = os.environ.get('JDV_USER')
+PASSWORD = os.environ.get('JDV_PASSWORD')
+HOST = os.environ.get('JDV_HOST') if os.environ.get('JDV_HOST') is not None else 'jdv.prod.a4.vary.redhat.com'
+PORT = os.environ.get('JDV_PORT') if os.environ.get('JDV_PORT') is not None else 35432
 config = {
-        'host': 'jdv.prod.a4.vary.redhat.com',
-        'port': 35432,
-        'user': 'amaheswa',
-        'password': 'Anirudh@25042015',
-        'database': 'marketing_vdm_dynamic',
+        'host': HOST,
+        'port': PORT,
+        'user': USER,
+        'password': PASSWORD,
+        'database': NAME,
         'sslmode': 'verify-ca',
-        'sslrootcert': r'C:\Users\amaheswa\Documents\flask-project\poc1\flask-test\Connection\root.crt'
+        'sslrootcert': ROOT_FILE
     }
